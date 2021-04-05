d3a67c87a46b3749bf4c993418da5563c706e138
nakachan1994 <nakachaaaan@gmail.com>
2021-04-05 02:50:18 +0000

    [finish]3-5

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,25 +4,25 @@ class BlogsController < ApplicationController
   end
 
   def show
-    @blog=Blog.find(params[])
+    @blog=Blog.find(params[:id])
   end
 
   def new
     @blog = Blog.new
   end
-  
+
   def create
     blog = Blog.new(blog_params)
     blog.save
-    redirect_to blogs_path
+    redirect_to blog_path(blog.id)
   end
 
   def edit
   end
-  
+
   private
   def blog_params
     params.require(:blog).permit(:title,:category,:body)
   end
-  
+
 end
